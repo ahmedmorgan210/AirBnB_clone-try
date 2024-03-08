ba3172e25b62541cdef696f50a878df7a3e8f9d6
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-03-08 19:33:29 +0000

    task 03

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
 #Base cls
+import uuid
+import datetime
 
 
 class BaseModel:
     """base is important"""
+    def __init__(self):
+       """hey it is ini"""
+       self.id = str(uuid.uuid4())
+       self.created_at = datetime.datetime.now()
+       self.updated_at = datetime.datetime.now()
+
+    def __str__(self) -> str:
+        """come to pri"""
+        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """rmmber me"""
+        self.updated_at = datetime.datetime.now()
+    def to_dict(self):
+        """open the book"""
+        str(datetime.datetime.isoformat(self.created_at))
+        str(datetime.datetime.isoformat(self.updated_at))
+        self.__dict__['__class__'] = __class__.__name__
+        return self.__dict__
+    
