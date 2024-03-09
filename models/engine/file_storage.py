a02684da2522252468b099b142a132f05e867b9b
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-03-09 16:43:08 +0000

    try to fix path

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,7 +7,7 @@ import os.path
 class FileStorage:
     """the store to by"""
     def __init__(self) -> None:
-        self.file_path = ""
+        self.file_path = "../../file.json"
         self.objects = {}
 
     @property
