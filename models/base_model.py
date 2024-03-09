fa38b339034d6792560d2b4fc0f35878ffb9c65e
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-03-09 09:49:44 +0000

    task 04

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,22 @@ import datetime
 
 class BaseModel:
     """base is important"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """hey it is ini"""
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+                #setattr(self, key, value)
+
 
     def __str__(self) -> str:
         """come to pri"""
