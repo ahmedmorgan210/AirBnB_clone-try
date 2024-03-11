2b0ce833800966fe1e9b3cade9128ddf37ca9f35
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-03-11 13:55:17 +0000

    update test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,15 +1,13 @@
-#!/usr/bin/python3
-
+import unittest
+"""testing base module"""
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class TestSBaseModels(unittest.TestCase):
+    """testing the base model"""
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+
+    def test_id(self):
+        self.assertIsInstance(self.my_model.id, str)
