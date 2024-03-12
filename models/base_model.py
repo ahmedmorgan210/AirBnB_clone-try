caae0c7c4881196bcac7377318b5a11653c33998
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-03-12 12:19:17 +0000

    try to save mo

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,28 @@
-#!/usr/bin/python3
-# Base cls
 import uuid
 import datetime
+import models
 
 
 class BaseModel:
     """base is important"""
+    date_time = datetime.datetime
+    #storage = models.storage
     def __init__(self, *args, **kwargs):
         """hey it is ini"""
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = BaseModel.date_time.now()
+        self.updated_at = BaseModel.date_time.now()
 
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
                 elif key == "created_at" or key == "updated_at":
-                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    value = BaseModel.date_time.strptime(value, "\
+                                                         %Y-%m-%dT%H:%M:%S.%f")
 
                 setattr(self, key, value)
-
 
     def __str__(self) -> str:
         """come to pri"""
@@ -29,7 +30,9 @@ class BaseModel:
 
     def save(self):
         """rmmber me"""
+        models.storage.save()
         self.updated_at = datetime.datetime.now()
+        #models.storage.save()
 
     def to_dict(self):
         """open the book"""
