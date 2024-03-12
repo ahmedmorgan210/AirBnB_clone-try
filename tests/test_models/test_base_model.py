cd46e28c3a376dab1a56801ce0727a68855be547
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-03-12 05:21:45 +0000

    task 03

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,7 @@
 import unittest
 """testing base module"""
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestSBaseModels(unittest.TestCase):
@@ -8,6 +9,22 @@ class TestSBaseModels(unittest.TestCase):
     my_model = BaseModel()
     my_model.name = "My First Model"
     my_model.my_number = 89
+    #time = datetime()
 
     def test_id(self):
         self.assertIsInstance(self.my_model.id, str)
+
+    def test_created_at(self):
+        self.assertIsInstance(self.my_model.created_at, object)
+
+    def test_save(self):
+        self.my_model.before_update = self.my_model.updated_at
+        self.my_model.name = "ahmed"
+        self.my_model.save()
+        self.my_model.after_update = self.my_model.updated_at
+        self.assertNotEqual(self.my_model.before_update, self.my_model.after_update)
+
+    def test_str(self):
+        n = self.my_model.__class__.__name__
+        expected_str = f"[{n}] ({self.my_model.id}) <{self.my_model.__dict__}>"
+        self.assertEqual(self.my_model.__str__(), expected_str)
